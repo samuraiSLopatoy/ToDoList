c2eb1a194af3ba0d094890ef2bb04b6795768c75
Michael <cawabungadude@ya.ru>
2021-09-27 10:29:32 +0300

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Михаил Кулагин on 27.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
